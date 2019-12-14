3466e5ce994542538f9f0739e0377f7933357f16
antug2001 <antug2001@gmail.com>
2019-12-14 12:41:32 +0530

    Camera controlling obtained
    1.Mouse scrolling based zooming
    2.Mouse click and drag based camera panning obtained

---
+++ SaladChefSimulation/Assets/scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject gameFacilitator;
+    private InputManager inputManager;
+    void Start()
+    {
+        inputManager = gameFacilitator.GetComponent<InputManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputManager.HandleCameraZoom();
+        inputManager.HandleCameraPan();
+    }
+}
---
+++ SaladChefSimulation/Assets/scripts/InputManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+
+    private float ZoomAmount = 0;
+    private float MaxToClamp = 10;
+    private float ROTSpeed = 1;
+    private Vector3 touchStart;
+    private float groundZ = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void HandleCameraZoom()//mouse wheel rotation based camera distance calculate within max-min position for zooming
+    {
+        ZoomAmount += Input.GetAxis("Mouse ScrollWheel");
+        ZoomAmount = Mathf.Clamp(ZoomAmount, -MaxToClamp, MaxToClamp);
+        float translate = Mathf.Min(Mathf.Abs(Input.GetAxis("Mouse ScrollWheel")), MaxToClamp - Mathf.Abs(ZoomAmount));
+        Camera.main.transform.Translate(0, 0, translate * ROTSpeed * Mathf.Sign(Input.GetAxis("Mouse ScrollWheel")));
+    }
+
+    public void HandleCameraPan()//mouse pointer pressed camera pan calculation
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            touchStart = GetWorldPosition(groundZ);
+        }
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 direction = touchStart - GetWorldPosition(groundZ);
+            Camera.main.transform.position += direction;
+        }
+    }
+
+    private Vector3 GetWorldPosition(float z)//world position obtained from screen point
+    {
+        Ray mousePos = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane ground = new Plane(Vector3.forward, new Vector3(0, 0, z));
+        float distance;
+        ground.Raycast(mousePos, out distance);
+        return mousePos.GetPoint(distance);
+    }
+
+}
